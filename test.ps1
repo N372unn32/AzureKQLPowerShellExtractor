fb8d7eaf5f733a1157ecad25c2f4e41b5111ac3d
Tushar Mudi <41691531+G-Lucifer@users.noreply.github.com>
2023-05-01 02:39:36 +0530

    kql error fixed

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
 Import-Module .\AzureKQLPowerShellExtractor.psm1 -Force -Verbose
 #Disconnect-AzAccount
 #Connect-AzAccount
-Search-AzGraph -Query "resourcecontainers" -Subscription "7edb96ee-28a1-4dab-96f7-a0374a9d5dc8"
-##Get-AzureKQLPowerShellExtract -kqlQuery .\samplekql.ps1
+Search-AzGraph -Query "resourcecontainers" 
+Get-AzureKQLPowerShellExtract -kqlQuery .\sample.kql
