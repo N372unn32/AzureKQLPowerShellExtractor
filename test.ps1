8664a4c4852e7641d3a36125a00db677a5f9d340
G-Lucifer <tushar03122@outlook.com>
2023-05-05 22:42:34 +0530

    error messages need to be fixed

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
 Import-Module .\AzureKQLPowerShellExtractor.psm1 -Force -Verbose
 #Disconnect-AzAccount
 #Connect-AzAccount
-#Search-AzGraph -Query "resourcecontainers" j
+#Search-AzGraph -Query "resourcecontainers" 
 Get-AzureKQLPowerShellExtract -kqlQuery .\sample.kql
